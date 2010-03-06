ba99d8874546a4d5c16d0d7573592fe542289871
dwaynemac <dwaynemac@gmail.com>
2010-03-06 20:55:44 -0300

    add Version methods

--- lib/cache_version.rb
+++ lib/cache_version.rb
@@ -29,16 +29,34 @@ module CacheVersion
   module Version
       # increments key by value (default 1) and returns new value
       # if key is nil, sets to 1
-      def self.increment(key)
+      def self.increment(key, by = 1)
         if (value = Rails.cache.read("Version:#{key}")).nil?
           Rails.cache.write("Version:#{key}", (value = 1))
         else
-          Rails.cache.write("Version:#{key}", (value = value + 1))
+          Rails.cache.write("Version:#{key}", (value = value + by))
         end
 
         return value
       end
 
+      # decrements key by value (default 1) and returns new value
+      # if key is nil, sets to 0
+      def self.decrement(key, by = 1)
+        if (value = Rails.cache.read("Version:#{key}")).nil?
+          Rails.cache.write("Version:#{key}", (value = 0))
+        else
+          Rails.cache.write("Version:#{key}", (value = value - by))
+        end
+
+        return value
+      end
+
+      # sets key value, defaults to 0.
+      def self.set(key, value=0)
+        return Rails.cache.write("Version:#{key}",value)
+      end
+
+      # gets key value, defaults to 0.
       def self.read(key)
         return Rails.cache.fetch("Version:#{key}"){0}
       end
